75d5a6b048453920ededa811e61e32d39f913c61
jiaxiangu <gujiaxian1997@gmail.com>
2021-05-31 00:17:45 +0900

    Initial Commit

---
+++ WasedaTime-iosapp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WasedaTime-iosapp
+//
+//  Created by 顧嘉賢 on 2021/05/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
