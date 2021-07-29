03d0a5edb55086566f86ede9e13a789836034dfa
DSniperCHEN <56672260+DSniperCHEN@users.noreply.github.com>
2021-07-29 11:39:47 +0900

    Add files via upload

--- WasedaTime-iosapp/timetable/TimeTableContentView.swift
+++ WasedaTime-iosapp/timetable/TimeTableContentView.swift
@@ -10,7 +10,82 @@ import SwiftUI
 
 struct TimetableContentView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            TermTitle()
+            HStack{
+                Column(content: "M")
+                Column(content: "T")
+                Column(content: "W")
+                Column(content: "T")
+                Column(content: "F")
+                Column(content: "S")
+            }
+        }
+    }
+}
+
+struct TermTitle: View {
+    @State var spring: Bool = true
+    
+    var body: some View {
+        HStack{
+            Button(action: {
+                spring = !spring
+            }, label: {
+                Image(systemName: "chevron.left.circle")
+            })
+            
+            Spacer()
+            if spring {
+                Text("Spring / Summer")
+            } else {
+                Text("Autumn / Winter")
+            }
+            Spacer()
+            
+            Button(action: {
+                spring = !spring
+            }, label: {
+                Image(systemName: "chevron.right.circle")
+            })
+        }
+        .padding(.horizontal)
+    }
+}
+
+struct Period: View {
+    var day: String
+    var time: String
+    
+    var body: some View {
+        ZStack{
+            Text(day)
+            Text(time)
+            Rectangle()
+                .fill(Color.white)
+            Text("dummy class")
+        }
+    }
+}
+
+struct Column: View {
+    var content: String
+    
+    var body: some View {
+        Form{
+            Section{
+                Text(content)
+            }
+            Section{
+                Period(day: content,time: "1")
+                Period(day: content,time: "2")
+                Period(day: content,time: "3")
+                Period(day: content,time: "4")
+                Period(day: content,time: "5")
+                Period(day: content,time: "6")
+                Period(day: content,time: "7")
+            }
+        }
     }
 }
 
